3b8da2e5e8ff739271f84446c0e4572fa4ad4bfa
MBosha <boshyn@gmail.com>
2017-12-27 09:32:48 +0300

    111

--- Arduino/Сервер ESP8266.ino
+++ Arduino/Сервер ESP8266.ino
@@ -1,11 +1,11 @@
--- Автор – Руи Сантос (Rui Santos)
--- Более подробно о проекте на http://randomnerdtutorials.com
--- Сервер ESP8266
+//-- Автор – Руи Сантос (Rui Santos)
+//-- Более подробно о проекте на http://randomnerdtutorials.com
+//-- Сервер ESP8266
  
-print("ESP8266 Server")  --  "Сервер ESP8266"
+print("ESP8266 Server")  //--  "Сервер ESP8266"
 wifi.setmode(wifi.STATIONAP);
 wifi.ap.config({ssid="test",pwd="12345678"});
-print("Server IP Address:",wifi.ap.getip())  --  "IP-адрес сервера:"
+print("Server IP Address:",wifi.ap.getip())  //--  "IP-адрес сервера:"
  
 sv = net.createServer(net.TCP)
 sv:listen(80, function(conn)
